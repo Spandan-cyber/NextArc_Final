3d0772822048aa33fc8210b5cc52b5f5a4a84a62
Spandan Das <119717681+Spandan-cyber@users.noreply.github.com>
2025-12-10 20:14:02 +0530

    Update Firebase configuration for new project

--- firebase-init.js
+++ firebase-init.js
@@ -1,12 +1,13 @@
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyAmvxUFd_efs5_FkEXIaaO3UWmeNygQeHU",
-  authDomain: "nextarc-opportunity-app.firebaseapp.com",
-  projectId: "nextarc-opportunity-app",
-  storageBucket: "nextarc-opportunity-app.firebasestorage.app",
-  messagingSenderId: "699056168349",
-  appId: "1:699056168349:web:7f947636e099c7463b6374",
-  measurementId: "G-MSMBPRN3BY"
+  apiKey: "AIzaSyBV_OhDVCc7qnD4t62RrWr8wV7UjQpnQbk",
+  authDomain: "nextarc-final-app.firebaseapp.com",
+  projectId: "nextarc-final-app",
+  storageBucket: "nextarc-final-app.firebasestorage.app",
+  messagingSenderId: "286053477372",
+  appId: "1:286053477372:web:3d992e900114164185e079",
+  measurementId: "G-BF8CDV1QQE"
 };
+
 firebase.initializeApp(firebaseConfig);
